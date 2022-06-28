0b2df778ca662c388f9dac6ee3fc61da68dc7088
ruknez <ruknez94@mail.ru>
2022-06-29 00:07:20 +0300

    fix get request

--- internal/workmanager/siteparser/siteParser.go
+++ internal/workmanager/siteparser/siteParser.go
@@ -27,9 +27,9 @@ func (s *siteParser) Calculate(url, word string) (int, error) {
 	ctx, cancel := context.WithTimeout(s.ctx, s.timeOut)
 	defer cancel()
 
-	resp, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	resp, err := doGetRequest(ctx, url)
 	if err != nil {
-		return 0, fmt.Errorf("cannot get %s %w", url, err)
+		return 0, err
 	}
 	defer resp.Body.Close()
 
@@ -38,4 +38,18 @@ func (s *siteParser) Calculate(url, word string) (int, error) {
 		return 0, fmt.Errorf("cannot ReadAll body %w", err)
 	}
 	return strings.Count(string(body), word), nil
+}
+
+// doGetRequest делает GET запрос, в случае успеха необходимо закрыть тело.
+func doGetRequest(ctx context.Context, url string) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot make create request %s %w", url, err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("cannot make do request %s %w", url, err)
+	}
+	return resp, nil
 }
